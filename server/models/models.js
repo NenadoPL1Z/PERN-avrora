9875fd4f179566d38605db8a0fdf5e6df3c5f225
Rodion Pyatigroskiy <81239058+NenadoPL1Z@users.noreply.github.com>
2022-02-20 18:05:49 +0500

    first commit

---
+++ server/models/models.js
@@ -0,0 +1,40 @@
+const sequilize = require('../db');
+const {DataTypes, Sequelize} = require('sequelize');
+
+const User = sequilize.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    email: {type: DataTypes.STRING, unique: true},
+    password: {type: DataTypes.STRING},
+    roles: {type: DataTypes.STRING, defaultValue: 'USER'},
+    firstName: {type: DataTypes.STRING},
+    lastName: {type: DataTypes.STRING}
+})
+
+const Vote = sequilize.define('vote', {
+    userId: {type: DataTypes.INTEGER},
+    postId: {type: DataTypes.INTEGER},
+    result: {type: DataTypes.ARRAY(DataTypes.INTEGER)}
+})
+
+const News = sequilize.define('news', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    title: {type: DataTypes.STRING, unique: true, allowNull: false},
+    subtitle: {type: DataTypes.STRING},
+    date: {type: DataTypes.FLOAT, allowNull: false},
+    img: {type: DataTypes.STRING, allowNull: false},
+})
+
+const PostType = sequilize.define('postType', {
+    id: {type: DataTypes.INTEGER, primaryKey: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false}
+})
+
+User.hasMany(Vote);
+Vote.belongsTo(User);
+
+News.hasOne(PostType);
+PostType.belongsTo(News);
+
+module.exports = {
+    User, Vote, News, PostType
+}
